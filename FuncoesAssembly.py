7920db5cc609a335f2ba0f0c16a440e683168a77
DanMO23 <115201102+DanMO23@users.noreply.github.com>
2023-04-22 15:01:46 -0300

    formatacoes feitas

--- FuncoesAssembly.py
+++ FuncoesAssembly.py
@@ -10,7 +10,11 @@ Opcode = {
 func3 = {
     'add':"000",
     'xor':"100",
-    'sll':"001"
+    'sll':"001",
+    'addi': "000",
+    'lw': "010",
+    'sw':"010",
+    'bne':"001"
 }
 
 func7 ={
@@ -70,12 +74,17 @@ def DefinirInstrução(arq_inst, i):
 
     return assemblyBin
 
+def DefinirTamanho(arq_inst):
+    tam = arq_inst.find(" ") 
+    tam = int(tam) +2
+    
 
+    return tam 
         
 
 
 def FormatR(arq_inst, func3, func7, tam_int):
-    j, rd, rs1, rs2 = tam_int+2, '', '', ''
+    j, rd, rs1, rs2 = tam_int, '', '', ''
     #j = tamanho da instrução + 2
     while (arq_inst[j] != ','):
         rd = rd + arq_inst[j]
@@ -94,18 +103,92 @@ def FormatR(arq_inst, func3, func7, tam_int):
     rs1 = "{0:05b}".format(rs1)
     rs2 = int(rs2)
     rs2 = "{0:05b}".format(rs2)
+
+    
     assemblyBin = func7 + rs2 + rs1 + func3 + rd + Opcode["R"]
     
     return assemblyBin
 
+def FormatI(arq_inst, func3,  tam_int):
+    j, rd, rs1, immediate = tam_int, '', '', ''
+    #j = tamanho da instrução + 2
+
+    #caso seja addi
+    if(arq_inst[0:(tam_int-2)] == "addi"):
+        while (arq_inst[j] != ','):
+            rd = rd + arq_inst[j]
+            j += 1
+        j += 3
+        while (arq_inst[j] != ','):
+            rs1 = rs1 + arq_inst[j]
+            j += 1
+        j += 2
+        if (arq_inst[j] == '-'):
+            negativo = True
+            j += 1
+        while (j != len(arq_inst)):
+            immediate = immediate + arq_inst[j]
+            j += 1
+        rd = int(rd)
+        rd = "{0:05b}".format(rd)
+        rs1 = int(rs1)
+        rs1 = "{0:05b}".format(rs1)
+        immediate = int(immediate)
+        if (negativo):
+            immediate = (pow(2, 12) - immediate)
+            negativo = False
+        immediate = "{0:012b}".format(immediate)
+        assemblyBin = immediate + rs1 + func3 + rd + Opcode["I"]
+        return assemblyBin
+
+    #caso nao seja addi
+    while (arq_inst[j] != ','):
+        rd = rd + arq_inst[j]
+        j += 1
+    j += 2
+    while (arq_inst[j] != '('):
+        immediate = immediate + arq_inst[j]
+        j += 1
+    j += 2
+    while (arq_inst[j] != ')'):
+        rs1 = rs1 + arq_inst[j]
+        j += 1
+    rd = int(rd)
+    rd = "{0:05b}".format(rd)
+    rs1 = int(rs1)
+    rs1 = "{0:05b}".format(rs1)
+    immediate = int(immediate)
+    immediate = "{0:012b}".format(immediate)
+    assemblyBin = immediate + rs1 + func3 + rd + Opcode["I"]
+    return assemblyBin
+    
+def FormatS(arq_inst,  func3, tam_int):
+    
+    j, rs2, rs1, immediate = tam_int, '', '', ''
+    while (arq_inst[j] != ','):
+        rs2 = rs2 + arq_inst[j]
+        j += 1
+    j += 2
+    while (arq_inst[j] != '('):
+        immediate = immediate + arq_inst[j]
+        j += 1
+    j += 2
+    while (arq_inst[j] != ')'):
+        rs1 = rs1 + arq_inst[j]
+        j += 1
+    rs2 = int(rs2)
+    rs2 = "{0:05b}".format(rs2)
+    rs1 = int(rs1)
+    rs1 = "{0:05b}".format(rs1)
+    immediate = int(immediate)
+    immediate = "{0:012b}".format(immediate)
+    assemblyBin = immediate[0:7] + rs2 + rs1 + func3 + immediate[7:12] + Opcode["S"]
+    return assemblyBin
 
 
-
-
-#Bne - Instrução no formato SB
-def OpBne(arq_inst):
-
-    j, rs2, rs1, immediate = 5, '', '', ''
+def FormatSB(arq_inst,  func3, tam_int):
+    
+    j, rs2, rs1, immediate = tam_int, '', '', ''
 
     
     while (arq_inst[j] != ','):
@@ -125,98 +208,46 @@ def OpBne(arq_inst):
     rs1 = "{0:05b}".format(rs1)
     immediate = int(immediate)
     immediate = "{0:012b}".format(immediate)
-    assemblyBin = immediate[0:7] + rs2 + rs1 + "001" + immediate[7:12] + "1100011"
+    assemblyBin = immediate[0:7] + rs2 + rs1 + func3 + immediate[7:12] + Opcode["SB"]
+    return assemblyBin
+
+
+#Bne - Instrução no formato SB
+def OpBne(arq_inst):
+    
+    assemblyBin = FormatSB(arq_inst, func3["bne"], DefinirTamanho(arq_inst))
     return assemblyBin
 
 #Sw - Instrução no formato S
 def OpSw(arq_inst):
-    j, rs2, rs1, immediate = 4, '', '', ''
-    while (arq_inst[j] != ','):
-        rs2 = rs2 + arq_inst[j]
-        j += 1
-    j += 2
-    while (arq_inst[j] != '('):
-        immediate = immediate + arq_inst[j]
-        j += 1
-    j += 2
-    while (arq_inst[j] != ')'):
-        rs1 = rs1 + arq_inst[j]
-        j += 1
-    rs2 = int(rs2)
-    rs2 = "{0:05b}".format(rs2)
-    rs1 = int(rs1)
-    rs1 = "{0:05b}".format(rs1)
-    immediate = int(immediate)
-    immediate = "{0:012b}".format(immediate)
-    assemblyBin = immediate[0:7] + rs2 + rs1 + "001" + immediate[7:12] + "0100011"
+    
+    assemblyBin = FormatS(arq_inst,func3["sw", DefinirTamanho(arq_inst)])
     return assemblyBin
 
 #Addi - Instrução no formato I
 def OpAddi(arq_inst):
-    j, rd, rs1, immediate = 6, '', '', ''
-    while (arq_inst[j] != ','):
-        rd = rd + arq_inst[j]
-        j += 1
-    j += 3
-    while (arq_inst[j] != ','):
-        rs1 = rs1 + arq_inst[j]
-        j += 1
-    j += 2
-    if (arq_inst[j] == '-'):
-        negativo = True
-        j += 1
-    while (j != len(arq_inst)):
-        immediate = immediate + arq_inst[j]
-        j += 1
-    rd = int(rd)
-    rd = "{0:05b}".format(rd)
-    rs1 = int(rs1)
-    rs1 = "{0:05b}".format(rs1)
-    immediate = int(immediate)
-    if (negativo):
-        immediate = (pow(2, 12) - immediate)
-        negativo = False
-    immediate = "{0:012b}".format(immediate)
-    assemblyBin = immediate + rs1 + "000" + rd + "0010011"
+    assemblyBin = FormatI(arq_inst, func3["addi"], DefinirTamanho(arq_inst))
     return assemblyBin
 
 #Lw - Instrução no formato I
 def OpLw(arq_inst):
-    j, rd, rs1, immediate = 4, '', '', ''
-    while (arq_inst[j] != ','):
-        rd = rd + arq_inst[j]
-        j += 1
-    j += 2
-    while (arq_inst[j] != '('):
-        immediate = immediate + arq_inst[j]
-        j += 1
-    j += 2
-    while (arq_inst[j] != ')'):
-        rs1 = rs1 + arq_inst[j]
-        j += 1
-    rd = int(rd)
-    rd = "{0:05b}".format(rd)
-    rs1 = int(rs1)
-    rs1 = "{0:05b}".format(rs1)
-    immediate = int(immediate)
-    immediate = "{0:012b}".format(immediate)
-    assemblyBin = immediate + rs1 + "010" + rd + "0000011"
+    assemblyBin = FormatI(arq_inst, func3["lw"], DefinirTamanho(arq_inst))
     return assemblyBin
 
 #Sll - Instrução no formato R
 def OpSll(arq_inst):
     
-    assemblyBin = FormatR(arq_inst, func3["sll"], func7["sll"], 3)
+    assemblyBin = FormatR(arq_inst, func3["sll"], func7["sll"], DefinirTamanho(arq_inst))
     return assemblyBin
 
 #Add - Instrução no formato R
 def OpAdd(arq_inst):
    
-    assemblyBin = FormatR(arq_inst, func3["add"], func7["add"], 3)
+    assemblyBin = FormatR(arq_inst, func3["add"], func7["add"], DefinirTamanho(arq_inst))
     return assemblyBin
 
 #Xor - Instrução no formato R
 def OpXor(arq_inst):
     
-    assemblyBin = FormatR(arq_inst, func3["xor"], func7["xor"], 3)
+    assemblyBin = FormatR(arq_inst, func3["xor"], func7["xor"], DefinirTamanho(arq_inst))
     return assemblyBin
